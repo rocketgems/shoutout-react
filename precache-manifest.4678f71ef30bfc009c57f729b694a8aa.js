180a79cab03da1aebae73c3260d9d0faa1c4a4db
Curtis Cummings <curtis@aquil.io>
2021-01-27 19:54:41 -0500

    Updates

---
+++ precache-manifest.4678f71ef30bfc009c57f729b694a8aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ee54afbd67303bd258c7f608d262d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "790e5cab19897b85f12b",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "108b8a8834be0f967681",
+    "url": "./static/js/2.17b69a17.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.17b69a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790e5cab19897b85f12b",
+    "url": "./static/js/main.cb950906.chunk.js"
+  },
+  {
+    "revision": "c929747e7e3996febbfc",
+    "url": "./static/js/runtime-main.04793024.js"
+  }
+]);
